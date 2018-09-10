b8d9c7ce7d2de5c39053047be2f95431ac14017a
Shiju John <shijujohn1093@gmail.com>
2018-09-11 00:43:37 +0530

    Created Launcher file

--- src/main/java/com/thengara/teamservice/endpoint/HealthController.java
+++ src/main/java/com/thengara/teamservice/endpoint/HealthController.java
@@ -1,41 +1,49 @@
 package com.thengara.teamservice.endpoint;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.thengara.teamservice.entity.Team;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
 import com.thengara.teamservice.service.TeamService;
 
 @RestController
 @RequestMapping("/team")
-public class TeamController {
+public class HealthController {
 
 	@Value("${test.env.key}")
 	private String envKey;
 	
-	@Autowired
+	
 	private final Environment environment;
 	
+
 	private final TeamService teamService;
 
-	public TeamController(TeamService teamService, Environment environment) {
+	@Autowired
+	public HealthController(TeamService teamService, Environment environment) {
 		this.teamService = teamService;
 		this.environment = environment;
 	}
 
-	@RequestMapping("/findateam")
-	public Team findATeam() {
-		return teamService.findAll().get(0);
-	}
 
-	@RequestMapping("/findbyname/{name}")
-	public Team findByName(@PathVariable String name) {
-		return teamService.findByName(name);
+	
+	@RequestMapping("/testKey")
+	public String testKey() {
+		
+		return envKey +"~"+environment.getProperty("test.env.key");
 	}
+	
 	
 
 
